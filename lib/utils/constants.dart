22b4be824ed6ea6f53150805a79bc3e085bd687b
Akash Giri <akx.sonu@gmail.com>
2023-11-08 10:26:08 +0530

    Change tnc

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -910,8 +910,64 @@ Thank you for being an eco-conscious customer and supporting our mission!
 Best regards
 ''';
 
+
+
   static String termsAndConditions = '''
- In part consideration for the Solar Water Heater(s), I, the customer agree to irrevocably and unconditionally transfer to M/s. Sudarshan Saur Shakti Pvt. Ltd., full legal and beneficial title to all & any rights relating to the reduction of Green House Gas emissions & environmental benefits arising from the installation & use of solar water heating system(s) including Voluntary Emission Reductions (VERs) under the Kyoto Protocol & Paris Agreement. I agree to provide M/s. Sudarshan Saur Shakti Pvt. Ltd., any necessary assistance to bring such rights into existence''';
+1. The Dealer is authorized to sell the products offered by the company.  The Dealer (or it’s representative) can communicate various technical and commercial information regarding the products of the Company to their prospective customers based on training and various literature supplied by the Company from time to time. Company will not be responsible for communication of wrong information & false commitments made or given by the Dealer or its representatives to customers/end user.
+
+
+2. Company will decide operating area for the Dealer and Dealer will have to restrict their operations within the area allocated to him only.
+
+
+3. The Dealer have to fulfill all legal formalities (with amendments from time to time) with the Company such as execution of Agreement, Security Deposit payment etc. if required.
+
+
+4. The Company will issue various information, circulars, letters, covering marketing strategy, price list, commission structure, incentive schemes, payment terms, etc. to “Dealer” from time to time. The same will be binding on Dealer.
+
+
+5. However, Company reserves the right to alter or change the financial terms and conditions, prices, commission structure etc. without prior notice to the Dealer.
+
+
+6. The Dealer will have to develop all necessary infrastructure for marketing, installations, and servicing of various products of Company as and when required.
+
+
+7. The Dealer will provide timely services for Sudarshan Saur's products to the customer as per the servicing and maintenance guidelines provided by the company.
+
+
+8. The Company products may be sold to user via two different “Sales Modes” depending on the profile of customer. The brief details are as under: -
+
+
+a) Dealer Sell Mode: - Wherein Company will raise the Invoices in the name of Dealer, and Dealer will sell the products to customer by raising his own Invoice.
+
+
+b) Customer Sell Mode: - Wherein company will raise the Invoice directly in the name of customer and Dealer will be paid the mutually decided sells commission after completion of project & after full recovery of payment from customer.
+
+
+However, in both the above cases full recovery of payment from customer and complete payment to Company against various invoices raised by Company i.e. (in case of dealer sell mode) & invoices raised to the customer (in case of customer sell mode) will be the sole responsibility of Dealer only.
+
+
+Any un-recovered amount from any customer against Company invoices for any reason will be debited fromdealer’s account.
+
+
+Company will charge the interest to Dealer on delayed payment recoveries as per the terms and conditions decided by the Company from time to time and the same will be binding on Dealer.  
+
+
+9. Dealer will have to furnish full details of customers like Permanent address where system is installed, Permanent Mobile no., Date of Installation, Photograph of the system with name plate, system capacity, Price of product, Google map location[ Geographical location of the swhs]etc. as per the formats furnished by Company from time to time.
+
+
+10. The Company shall issue the account statement to the Dealer as & when required.  The Dealer has to acknowledge the same. If the dealer does not acknowledge the same within the period of 15 days from the date of issue, it shall be deemed by default that the dealer has acknowledged and accepted the account statement.
+
+
+11. The Company, or their representative have full right to audit anytime all the working pattern of Dealer, which may cover marketing, installation, servicing & financial aspects.  The Dealer will provide free access to all such information to Company officials and extend full co. operation and support related to the third-party audits of installed systems during survey.
+
+
+12. It is strictly understood by both/all the parties or in between the customers & both/ all the parties that the Jurisdiction of all the disputes (Civil/Criminal or otherwise) which may arise in between Company and customer or between company and any ofthe parties, shall be at Aurangabad where the Company is having its registered office.
+
+
+13. Dealer will convey / inform to sub dealers/ salespersonall terms and conditions of the company related to, but not limited to, the giving service to the customer, and following all instruction given in user manual or in various circulars issued by company time to time.
+
+14. Company also reserves right to terminate this Agreement without assigning any reason.
+ ''';
 
   static String guaranteeCardMsg =
       '''"in part consideration for the Solar Water Heater(s), I, the customer agree to irrevocably and unconditionally transfer to M/s. Sudarshan Saur Shakti Pvt Ltd., full legal and beneficial title to all & any rights relating to the reduction of Green House Gas emissions & environmental benefits arising from the installation & use of solar water heating system(s) including Voluntary Emission Reductions (VERs) under the Kyoto Protocol & Paris Agreement. I agree to provide M/s. Sudarshan Saur Shakti Pvt Ltd., any necessary assistance to bring such rights into existence"''';
